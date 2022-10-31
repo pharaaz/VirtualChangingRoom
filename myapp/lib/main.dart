911abed0f63d36c530ffbc41ef19b1d3b6b7af0a
pharaaz <80433979+pharaaz@users.noreply.github.com>
2022-11-01 00:12:11 +1300

    added overlay image

--- myapp/lib/main.dart
+++ myapp/lib/main.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:myapp/routes.dart';
 import 'package:myapp/theme.dart';
+import 'package:myapp/tryon/tryon.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
--- myapp/lib/tryon/tryon.dart
+++ myapp/lib/tryon/tryon.dart
@@ -1,12 +1,21 @@
 import 'package:flutter/material.dart';
+import 'package:camera/camera.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
-class TryOnScreen extends StatelessWidget {
-  const TryOnScreen({Key? key}) : super(key: key);
+class TryOnScreen extends StatefulWidget {
+  const TryOnScreen({super.key});
 
   @override
+  State<TryOnScreen> createState() => _TryOnScreenState();
+}
+
+class _TryOnScreenState extends State<TryOnScreen> {
+  @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(),
+    return Column(
+      children: [
+        Image.asset('assets/images/lbj23.jpeg'), // <-- SEE HERE
+      ],
     );
   }
 }
